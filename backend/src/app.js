57e11a52fff3fdb1efaf5932ef7c34fce828a268
Akshata Bhoi <akshatabbhoi@gmail.com>
2026-03-15 14:56:05 +0530

    Render fix For backend Deployment

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,6 +10,13 @@ const app = express()
 app.use(cors())
 app.use(express.json({ limit: "50mb" }))
 
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "https://quick-cv-xi.vercel.app"
+  ],
+  credentials: true
+}));
 app.get("/api/health", (_, res) => {
     res.status(200).json({ Status: "OK" })
 })

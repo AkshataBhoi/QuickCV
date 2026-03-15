d4d964be77050e67cdefcbaf5e61f678fe503cbc
Akshata Bhoi <akshatabbhoi@gmail.com>
2026-03-15 16:06:12 +0530

    deplyment fixes

--- app/_not-found.tsx
+++
@@ -1,9 +0,0 @@
-// // app/_not-found.tsx
-// import dynamic from 'next/dynamic';
-
-// const NotFoundContent = dynamic(
-//   () => import('./NotFoundContent'), 
-//   { ssr: false }
-// );
-
-// export default NotFoundContent;
--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,18 @@ import connectDB from "./config/db.js"
 
 connectDB()
 
+import cors from "cors";
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://quick-cv-xi.vercel.app"
+    ],
+    credentials: true
+  })
+);
+
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
     console.log(`Server Running on Port ${PORT}`)
